d8738f3c394b2d61667ffe34e02beb971f125583
Kacper Krasowiak <krasowiak@google.com>
2024-02-06 09:01:27 -0800

    No public description
    PiperOrigin-RevId: 604664666
    Change-Id: I3ac8afd5bcae49f7523b0a0e425f61c7ef61c6c2

--- optimus/__init__.py
+++ optimus/__init__.py
@@ -11,3 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+"""Optimus library for real-time marketing personalization using RL."""
+
+__version__ = "0.0.1"
--- setup.py
+++ setup.py
@@ -13,30 +13,69 @@
 # limitations under the License.
 
 """The setup script for Optimus."""
-import pathlib
+
+import os
+from typing import Final
 import setuptools
 
-with pathlib.Path("requirements.txt").open() as requirements_path:
-  requirements = requirements_path.read().splitlines()
+_CURRENT_DIR: Final[str] = os.path.dirname(os.path.abspath(__file__))
+
+
+def _get_readme():
+  try:
+    readme = open(
+        os.path.join(_CURRENT_DIR, "README.md"), encoding="utf-8"
+    ).read()
+  except OSError:
+    readme = ""
+  return readme
+
+
+def _get_version():
+  with open(os.path.join(_CURRENT_DIR, "optimus", "__init__.py")) as fp:
+    for line in fp:
+      if line.startswith("__version__") and "=" in line:
+        version = line[line.find("=") + 1 :].strip(" '\"\n")
+        if version:
+          return version
+    raise ValueError("`__version__` not defined in `optimus/__init__.py`")
+
+
+def _parse_requirements(path):
+  with open(os.path.join(_CURRENT_DIR, path)) as f:
+    return [
+        line.rstrip()
+        for line in f
+        if not (line.isspace() or line.startswith("#"))
+    ]
+
+
+_VERSION: Final[str] = _get_version()
+_README: Final[str] = _get_readme()
+_INSTALL_REQUIREMENTS: Final[str] = _parse_requirements(
+    os.path.join(_CURRENT_DIR, "requirements.txt")
+)
+
 
 setuptools.setup(
     name="optimus",
-    version="0.0.1",
+    version=_VERSION,
+    python_requires=">=3.10",
+    description=(
+        "Optimus library for real-time marketing personalization using RL."
+    ),
+    long_description=_README,
+    long_description_content_type="text/markdown",
     author="Google gTech Ads EMEA Privacy Data Science Team",
     license="MIT",
-    packages=setuptools.find_packages(
-        include=[
-            "optimus",
-            "optimus.*",
-        ]
-    ),
-    install_requires=requirements,
-    extras_require={},
+    packages=setuptools.find_packages(),
+    install_requires=_INSTALL_REQUIREMENTS,
+    url="https://github.com/google-marketing-solutions/optimus",
+    keywords="1pd privacy reinforcement learning optimization personalization",
     classifiers=[
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: Apache Software License",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
-    keywords="1pd privacy reinforcement learning optimization personalization",
 )
